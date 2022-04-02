3e5ed451f26b97e5df670e221bbd9a1bd5ba6dec
Dmitriy <Dzimitriy@mail.ru>
2022-04-02 20:47:08 +0300

    AuthorizationFilter add.

--- src/main/java/web/filter/AuthorizationFilter.java
+++ src/main/java/web/filter/AuthorizationFilter.java
@@ -1,4 +1,22 @@
 package web.filter;
 
-public class AuthorizationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(servletNames = {"CalculationServlet","CalculationHistoryServlet"})
+public class AuthorizationFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        if(req.getSession().getAttribute("user")==null){
+            res.sendError(HttpServletResponse.SC_FORBIDDEN);
+        }else{
+            chain.doFilter(req,res);
+        }
+    }
 }
