61c0efd1d9d7d347a25687977d679556de03e4cd
HAYEL <i.hayel.dev@gmail.com>
2022-04-11 23:25:19 +0300

    Cover UserService with logs.

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,10 +1,12 @@
 package service;
 
 import entity.User;
+import lombok.extern.slf4j.Slf4j;
 import storage.EntityStorage;
 
 import java.util.List;
 
+@Slf4j
 public class UserService {
     private static final EntityStorage<User> userStorage = new EntityStorage<>();
 
@@ -19,14 +21,17 @@ public class UserService {
     }
 
     public boolean save(User user) {
+        log.info("User with id " + user.getId() + " saved");
         return userStorage.save(user);
     }
 
     public User find(int id) {
+        log.info("Searching user with id " + id);
         return userStorage.find(id);
     }
 
-    public User find(String login){
+    public User find(String login) {
+        log.info("Searching user with login " + login);
         return userStorage.findAll().stream()
                 .filter(user -> user.getLogin().equals(login))
                 .findFirst()
@@ -35,14 +40,17 @@ public class UserService {
     }
 
     public boolean remove(int id) {
+        log.info("User with id " + id + " removed");
         return userStorage.remove(id);
     }
 
     public User update(User user) {
+        log.info("User with id " + user.getId() + " updated");
         return userStorage.update(user);
     }
 
     public boolean isLoginExist(String login) {
+        log.info("Checking if login " + login + " exist");
         List<User> users = userStorage.findAll();
         return users.stream()
                 .anyMatch(user -> user.getLogin().equals(login));
